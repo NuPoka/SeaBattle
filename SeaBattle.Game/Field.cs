eda99b3b4269fb74f3c1c29eb68ec0ad42e19721
731-20 <731-20@C41412>
2022-10-01 13:01:24 +0300

    Добавьте файлы проекта.

---
+++ SeaBattle.Game/Field.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SeaBattle.Game
+{
+    public class Field
+    {
+        private byte[,] cells, SetCell;
+        public byte GetCellsValue(byte x, byte y)
+        {
+            return cells[x, y];
+        }
+        public bool SetCellsValue(byte x, byte y, byte z)
+        {
+            if (cells[x, y] != 0)
+            {
+                return false;
+            }
+            cells[x, y] = z;
+            return true;
+        }
+        public Field(int sizex) : this(sizex, sizex)
+        {
+
+        }
+        public Field(int sizex, int sizey)
+        {
+            cells = new byte[sizex, sizey];
+
+        }
+        public Field() : this(10)
+        {
+
+        }
+        public bool CrashValue(byte x, byte y)
+        {
+            if (cells[x, y] == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
